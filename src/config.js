21ba15c3a36db5e5462bc300e4b7222086309b13
DCK <dogancihatkayanews@gmail.com>
2023-04-20 12:12:02 +0300

    2 versiyon eklendi

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   api: {
     // API_URL: "https://api-node.themesbrand.website",
-    API_URL: "http://25.70.54.23/api/",
+    API_URL: "http://144.126.137.33:8081/api/",
   },
   title: {
     name: "Veri Analizi ve Yönetimi Medyaizi"
@@ -18,7 +18,7 @@ module.exports = {
     symbol: "₺"
   },
   backend_api: {
-    url:"http://25.70.54.23/api/"
+    url:"http://144.126.137.33:8081/api/"
   }
 };
 
